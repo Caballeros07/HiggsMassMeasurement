9a4aa2fcbf639cbc3c0926a6b495e2f23d5c084f
Cristofer Alexander Caballeros <cris.caballeros@melrose.ihepa.ufl.edu>
2019-06-19 18:51:33 -0400

    Pushing updated Invaraint mass of 2 bodies decaying from one parent particle.

--- deltamZCalc.py
+++ deltamZCalc.py
@@ -4,15 +4,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #macros that I pull from my functions.py
-from functions import DotProduct, Mag, Inv
-
+from functions import * 
 
 #definition of the file
 path = "/raid/raid8/ferrico/HZZ4l/CMSSW_10_2_5/src/leptonPtErrorCorrector/makeSlimTree/output/DY_2018/DYJetsToLL_M-50_kalman_v4_m2e_v2.root"
-
-#defintion of the bin size, to cut the histo
-binedgeMass = [60,65,70,75,80,85,90,95,100,105,110,115,120]
-
 
 #---------------------------------------------------------------------------------
 #branches being pulled and that will be operated on
@@ -29,62 +24,45 @@ eta2 = root2array(path,"passedEvents","eta2")
 phi1 = root2array(path,"passedEvents","phi1")  
 phi2 = root2array(path,"passedEvents","phi2")  
 
+#global lists being defined to be filled on a per event basis
 mZHist = []
-y=[]
-y2=[]
+deltamZHist = []
 closure = []
-#testing invariant masses for first 10 entries
-for i in range(1000):
-	p1 = Mag(pT1[i],phi1[i],eta1[i])
-	p2 = Mag(pT2[i],phi2[i],eta2[i])
-	
-	p1p2 = DotProduct(pT1[i],pT2[i],phi1[i],phi2[i],eta1[i],eta2[i])
-	
-	mZ = Inv(p1*p2, p1p2)
+
+#loop to set the do the calculations on a per event level. i is set as the number of events
+for i in range(10):
+	mZ = InvariantMass(pT1[i],pT2[i],phi1[i],phi2[i],eta1[i],eta2[i])
 	mZHist.append(mZ)
 	#closure test for massZ vs. mZ
 	close = mZ/massZ[i]
 	closure.append(close)
-
 	#calculation for deltamZ changing only the pT of the first lepton, pT1
-	p1prime = Mag(pT1[i]+pterr1[i],phi1[i],eta1[i])
-
-	p1p2prime = DotProduct(pT1[i]+pterr1[i],pT2[i],phi1[i],phi2[i],eta1[i],eta2[i])
-	
-	mZprime = Inv(p1prime*p2, p1p2prime)		
-	
-	deltamZ = mZprime - mZ
-	
-	y.append(deltamZ)
-
+	mZprime1 = InvariantMass(pT1[i]+pterr1[i],pT2[i],phi1[i],phi2[i],eta1[i],eta2[i])	
+	deltamZ1 = mZprime1 - mZ
 	#calculation for deltamZ changing only the pT of the second lepton, pT2
-	p2prime = Mag(pT2[i]+pterr2[i],phi2[i],eta2[i])
-	
-	p1p2prime2 = DotProduct(pT1[i],pT2[i]+pterr2[i],phi1[i],phi2[i],eta1[i],eta2[i])
-
-	mZprime2 = Inv(p2prime*p1, p1p2prime2)		
-	
+	mZprime2 = InvariantMass(pT1[i],pT2[i]+pterr2[i],phi1[i],phi2[i],eta1[i],eta2[i])	
 	deltamZ2 = mZprime2 - mZ
-	
-	y2.append(deltamZ)
+	#calculation for the total mZ for the entire event
+	mass_2l = Quad(deltamZ,deltamZ2)
+	deltamZHist.append(mass_2l)	
 
 
 #histograms of the mass of the Z boson, both calculated and branch
 fig2, cx = plt.subplots()
-cx.hist(massZ[0:1000], bins=30, color='red', fill=False) 
-cx.hist(mZHist, bins=30, color='green', fill=False) 
+cx.hist(massZ[0:10], bins=10, color='red', fill=False) 
+#cx.hist(mZHist, bins=30, color='green', fill=False) 
 cx.set_title("Invariant mass of the 2 leptons with delta pT smearing")
 cx.set_ylabel("Counts")
-cx.set_xlabel("mass 2l (GeV)")
+cx.set_xlabel("mass_2l (GeV)")
 fig2.savefig('../../public_html/MC_massZ_histo.png')
 
 
 
-
-#histogram of the delta mZ values when only changing the pT of the first lepton, pT1
+'''
+#histogram of the delta mZ values against the original values 
 fig1, bx = plt.subplots()
-bx.hist(y, binedgeMass, color='red', fill=False)
-bx.hist(y2, binedgeMass, color='blue', fill=False)
+bx.hist(massZ[0:1000], bins=30, color='red', fill=False)
+bx.hist(deltamZHist, bins=30, color='blue', fill=False)
 bx.set_title("Invariant mass of the 2 leptons with delta pT smearing")
 bx.set_ylabel("Counts")
 bx.set_xlabel("mass 2l (GeV)")
@@ -100,7 +78,7 @@ ax.set_ylabel("massZ_NTuple / massZ_calculated")
 ax.set_xlabel('Event Number')
 ax.scatter(x,closure)
 fig.savefig('../../public_html/MC_MassZ_closure_test.png')
-
+'''
 
 
 
--- functions.py
+++ functions.py
@@ -15,6 +15,36 @@ def Mag(pT,phi,eta):
 def Inv(a,b):
     return m.sqrt(2*(a-b))
 
+#definition for adding in quadrature:
+def Quad(a,b):
+	return m.sqrt(a**2+b**2)
 
 
 
+#definition to the entire calculation for the invariant mass
+def InvariantMass(pt1,pt2,phi1,phi2,eta1,eta2):
+	p1 = Mag(pt1,phi1,eta1)
+	p2 = Mag(pt2,phi2,eta2)
+	p1p2 = DotProduct(pt1,pt2,phi2,phi2,eta1,eta2)
+	mass = Inv(p1*p2, p1p2)
+	return mass
+
+
+
+
+
+
+'''
+#TO TEST THE FUNCIONS:
+pT1 = (1,2,3,4)
+pT2 = (1,2,3,4)
+phi1= (4,2,3,3)
+phi2= (8,6,3,4)
+eta1= (9,2,3,4)
+eta2= (3,2,7,4)
+
+i = 0
+
+
+print(InvariantMass(pT1[i],pT2[i],phi1[i],phi2[i],eta1[i],eta2[i]))
+'''
