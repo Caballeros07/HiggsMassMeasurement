36d3dbf7214d0bc59c7fd5e178488a1b9b9ebe46
Cristofer Alexander Caballeros <cris.caballeros@melrose.ihepa.ufl.edu>
2019-07-01 22:02:57 -0400

    Working version for the two body decay

--- deltamZCalc.py
+++ deltamZCalc.py
@@ -23,21 +23,19 @@ pterr2 = root2array(path,"passedEvents","pterr2")
 eta1 = root2array(path,"passedEvents","eta1")  
 eta2 = root2array(path,"passedEvents","eta2")  
 phi1 = root2array(path,"passedEvents","phi1")  
-phi2 = root2array(path,"passedEvents","phi2")  
-
+phi2 = root2array(path,"passedEvents","phi2") 
 
 #code to get the number of events in the NTuple
 f = TFile.Open("/raid/raid8/ferrico/HZZ4l/CMSSW_10_2_5/src/leptonPtErrorCorrector/makeSlimTree/output/DY_2018/DYJetsToLL_M-50_kalman_v4_m2e_v2.root")
 t = f.Get("passedEvents")
 n = t.GetEntries()
-print('Number of Events', n)
+print(n)
 
 #global lists being defined to be filled on a per event basis
 mZHist = []
 deltamZHist = []
 deltamZ = []
 closure = []
-n = 1000
 
 #loop to set the do the calculations on a per event level. i is set as the number of events
 for i in range(n):
@@ -57,8 +55,7 @@ for i in range(n):
     deltamass_2l = Quad(deltamZ1,deltamZ2)
     deltamZ.append(deltamass_2l)    
     deltamZHist.append(mass_2l)
-
-
+    
 #constants being used
 x = range(1,n+1) 
 l = np.linspace(0,130,130)
@@ -68,19 +65,17 @@ horiz_line_data = np.array([1 for i in xrange(len(x))])
 
 #scatterplot doing the closure test
 fig, ax = plt.subplots()
-ax.set_xlim(0,120)
-ax.set_ylim(0,120)
-ax.scatter(mZHist,massZ[0:n],color='blue',s=7,alpha=.8)
+ax.set_xlim(60,120)
+ax.set_ylim(60,120)
+ax.scatter(massZ[0:1000],mZHist[0:1000],color='blue',s=7,alpha=.8)
 ax.plot(l,l,'--',color='black')
 ax.plot(l,lshiftup,'--',color='black')
 ax.plot(l,lshiftdown,'--',color='black')
 #lableling
 ax.set_title('Closure Test')
-ax.set_ylabel("NTuple Mass(GeV)")
-ax.set_xlabel('Calculated Mass(GeV)')
+ax.set_xlabel("NTuple Mass(GeV)")
+ax.set_ylabel('Calculated Mass(GeV)')
 fig.savefig('../../public_html/MC_MassZ_closure_test.png')
-
-
 
 #histograms for the variation of the pT
 fig1, bx = plt.subplots()
@@ -91,41 +86,25 @@ bx.set_ylabel("Counts")
 bx.set_xlabel("mass 2l (GeV)")
 fig1.savefig('../../public_html/MC_deltamZ_varying_pT_histo.png')
 
-
-
 #histograms of the mass of the Z boson, both calculated and branch
 fig2, cx = plt.subplots()
-cx.set_xlim(0,200)
-cx.hist(massZ[0:n], bins=100, edgecolor='red', fill=False, histtype="step") 
-cx.hist(mZHist, bins=100, edgecolor='blue', fill=False, histtype="step") 
-cx.set_title("Invariant mass of the 2 leptons with delta pT smearing")
+cx.hist(massZ[0:n], bins=100, edgecolor='red',range=[0,200], fill=False, histtype="step") 
+cx.hist(mZHist, bins=100, edgecolor='blue',range=[0,200], fill=False, histtype="step") 
+cx.set_title("Invariant mass with delta pT smearing")
 cx.set_ylabel("Counts")
 cx.set_xlabel("mass_2l (GeV)")
-plt.show()
 fig2.savefig('../../public_html/MC_massZ_combined_histo.png')
 
-#scatterplot doing the variance
+#scatterplot doing the smeared closure test
 fig3, ax2 = plt.subplots()
-ax2.set_xlim(0,n)
-ax2.plot(x,horiz_line_data,color='red')
-ax2.scatter(x,deltamZ,color='blue',s=0.5,alpha=.8)
+ax2.set_xlim(60,120)
+ax2.set_ylim(60,120)
+ax2.scatter(massZ[0:1000],deltamZHist[0:1000],color='blue',s=7,alpha=.8)
+ax2.plot(l,l,'--',color='black')
+ax2.plot(l,lshiftup,'--',color='black')
+ax2.plot(l,lshiftdown,'--',color='black')
 #lableling
-ax2.set_title('Mass Variance')
-ax2.set_ylabel("Mass(Gev)")
-ax2.set_xlabel('Event Number')
-fig3.savefig('../../public_html/MC_MassZ_variance.png')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
+ax2.set_title('Closure Test')
+ax2.set_xlabel("NTuple Mass(GeV)")
+ax2.set_ylabel('Smeared Mass(GeV)')
+fig3.savefig('../../public_html/MC_MassZ_closure_smeared.png')
